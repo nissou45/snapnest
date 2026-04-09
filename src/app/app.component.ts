8ee1f5c64c037aaebbcbe79b1f06807618c29120
nissou45 <niss91@icloud.com>
2026-04-09 09:36:26 +0200

    feat: add scss styling for snap cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,25 @@ import { FaceSnap } from './models/snap.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Salon Bohème',
-      'Un coin lecture chaleureux avec des plantes, des coussins en velours et une lumière tamisée.',
-      'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?w=800',
-      new Date(),
-      10,
-    );
-    this.mySnap.setLocation('Paris');
-    this.myOtherSnap = new FaceSnap(
-      'Bureau Minimaliste',
-      'Un espace de travail épuré avec une belle lumière naturelle.',
-      'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?w=800',
-      new Date(),
-      156,
-    );
-    this.myOtherSnap.setLocation('à la montagne');
+    this.faceSnaps = [
+      new FaceSnap(
+        'Salon Bohème',
+        'Un coin lecture chaleureux avec des plantes, des coussins en velours et une lumière tamisée.',
+        'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?w=800',
+        new Date(),
+        10,
+      ),
+      new FaceSnap(
+        'Bureau Minimaliste',
+        'Un espace de travail épuré avec une belle lumière naturelle.',
+        'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?w=800',
+        new Date(),
+        156,
+      ),
+    ];
+    this.faceSnaps[0].setLocation('Paris');
+    this.faceSnaps[1].setLocation('à la montagne');
   }
 }

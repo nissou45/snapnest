b57dce3a0a70b72714be5f18335354ef7630c7b7
nissou45 <niss91@icloud.com>
2026-04-20 13:26:19 +0200

    feat(models): ajout de SnapType et refactoring de FaceSnap

--- src/app/models/snap.model.ts
+++ src/app/models/snap.model.ts
@@ -1,15 +1,27 @@
+import { SnapType } from './snap-type-type';
+
 export class FaceSnap {
   location?: string;
-
+  id: string;
   constructor(
     public title: string,
     public description: string,
     public imageUrl: string,
     public createAt: Date,
     public snaps: number,
-  ) {}
+  ) {
+    this.id = crypto.randomUUID().substring(0, 8); //UUID= universally unique id
+  }
   addSnap(): void {
     this.snaps++;
+  }
+
+  snap(snapType: SnapType) {
+    if (snapType === 'snap') {
+      this.addSnap();
+    } else {
+      this.removeSnap();
+    }
   }
   removeSnap(): void {
     this.snaps--;
